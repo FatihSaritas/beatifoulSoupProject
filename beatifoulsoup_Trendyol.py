eb491b3988f9b7ef36c436daccb66e10dbd0dcec
Fatih Sarıtaş <141572844+FatihSaritas@users.noreply.github.com>
2024-03-28 04:32:26 +0300

    Add files via upload

---
+++ beatifoulsoup_Trendyol.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+# url = ('https://www.trendyol.com/apple/iphone-11-128-gb-beyaz-cep-telefonu-aksesuarsiz-kutu-apple-turkiye-garantili-p-64074794?boutiqueId=61&merchantId=104870')
+
+# sayfa = requests.get(url)
+
+# html_sayfa = BeautifulSoup(sayfa.content,'html.parser')
+
+# isim = html_sayfa.find('h1', class_ ='pr-new-br').getText()
+
+# print(isim)
+
+# fiyat = html_sayfa.find('span', class_ ='prc-dsc').getText()
+# print(fiyat)
+
+#---------------------------------------------------------------------------------
